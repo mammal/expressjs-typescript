3bea2737cad21667c9ff6a4eb18f3895632933cf
memo.c <memo.c.tcg@gmail.com>
2022-06-05 19:51:03 +0800

    modify something

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,23 @@
 import dotenv from "dotenv";
 import express from "express";
 import path from "path";
+import cors from 'cors';
 import * as routes from "./routes";
 
 dotenv.config();
 
-const port = process.env.SERVER_PORT;
+const port = process.env.SERVER_PORT || 8080;
 const app = express();
 
 app.use(express.json());
-
+//  防止CORS
+app.use(cors());
+//  設定模板引擎
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-
+//  設定靜態資源路徑
 app.use(express.static(path.join(__dirname, "public")));
-
-// Configure routes
+//  註冊router
 routes.register(app);
 
 // start the express server
